1c699c23681ac20727828e4adc194d7bb7d509be
Pom94 <pamelavictoria-11@hotmail.com>
2023-08-02 18:40:33 -0300

    Funcion editarPelicula agregada y se agregaron comentarios al Json de peliculas

--- app.py
+++ app.py
@@ -20,6 +20,7 @@ def iniciarSesion():
         return jsonify({'mensaje': 'Usuario o contraseña incorrecta.'})
     
     
+    
 
 """___AGREGAR PELICULAS___"""
 
@@ -44,6 +45,8 @@ def agregarPelicula():
             return jsonify({'mensaje': 'Su pelicula ha sido agregada'}, peliculas)
     
     return jsonify({'mensaje': 'El director o genero de la pelicula no se encuentra.'})
+
+
     
 
 """___DEVOLVER PELICULAS POR DIRECTOR___"""
@@ -55,6 +58,41 @@ def peliculasDirector(director):
     peliculasDirector = [pelicula for pelicula in peliculas if pelicula['director'] == director]
 
     return jsonify(peliculasDirector)
+
+    
+
+
+"""___EDITAR PELICULA___"""        
+@app.route('/editar/<string:titulo>', methods=['PUT'])    
+def editarPelicula(titulo):
+    peliculaEditar = request.get_json()
+
+    with open('data/peliculas.json', 'r') as f:
+        peliculas  = json.load(f)
+    with open('data/directores.json', 'r') as f:
+        directores  = json.load(f)
+    with open('data/generos.json', 'r') as f:
+        generos  = json.load(f)
+    
+    for pelicula in peliculas:
+        if 'titulo' not in peliculaEditar or 'anio' not in peliculaEditar or 'director' not in peliculaEditar or 'genero' not in peliculaEditar or 'sinopsis' not in peliculaEditar or 'imagen' not in peliculaEditar:
+            return jsonify({'mensaje': 'Faltan datos de la pelicula.'})
+        elif peliculaEditar['director'] not in directores or peliculaEditar['genero'] not in generos :
+            return jsonify({'mensaje': 'El director o genero de la pelicula no se encuentra.'})
+        elif pelicula['titulo'] == titulo and len(pelicula['comentarios']) > 0:
+            return jsonify({'mensaje': 'No se puede editar pelicula que contengan comentarios ya hechos.'})
+        elif pelicula['titulo'] == titulo and peliculaEditar['titulo'] == titulo:
+            pelicula = peliculaEditar
+            return jsonify({'mensaje': 'La pelicula fue editada.'}, pelicula)
+        else:
+            return jsonify({'mensaje': 'No se encontro pelicula'})
+
+    
+        
+
+
+
+
 
     
     
